d0b4f378948b01ae467a3c70ae6db5e1c8e3f1e8
root <root@DesktopRobertt>
2023-06-24 14:31:30 -0300

    :sparkles: criado função para verificar e mostrar na tela se a senha é forte

--- script.js
+++ script.js
@@ -25,10 +25,37 @@ function gerarSenha(){
     }
 
     document.querySelector("#senhaGerada").value = senha;
+
+
+
+    const forcaSenha = document.querySelector('.forcaSenha');
+
+    const senhaForte = verificarSenhaForte(senha);
+
+    if(senhaForte){
+        forcaSenha.innerHTML = `<p class="forcaSenha" style="background: rgb(9, 180, 52); width: 150px; text-align: center;
+        position: relative; left: 41%;  color: #fff; border-radius: 3px; margin-bottom: 5px;" >Senha Forte</p>`
+        console.log('a senha e forte')
+    }else{
+        forcaSenha.innerHTML = `<p class="forcaSenha" style="background: red; width: 150px; text-align: center;
+        position: relative; left: 41%; color: #fff; border-radius: 3px; margin-bottom: 5px;" >Senha Fraca</p>`
+        console.log('a senha e fraca')
+    }
 }
 
 // função para atualizar um tempo real o valor do comprimento da senha
 function atualizarComprimento() {
     const comprimento = document.getElementById('rngComprimento').value;
     document.getElementById('ComprimentoAtual').textContent = comprimento;
+  }
+
+
+  // verificar se a senha é forte
+  function verificarSenhaForte(senha){
+    const possuiLetraMinuscula = /[a-z]/.test(senha);
+    const possuiLetraMaiuscula = /[A-Z]/.test(senha);
+    const possuiNumero = /[0-9]/.test(senha);
+    const possuiCaractereEspecial = /[^a-zA-Z0-9]/.test(senha);
+
+    return possuiLetraMinuscula && possuiLetraMaiuscula && possuiNumero && possuiCaractereEspecial;
   }
